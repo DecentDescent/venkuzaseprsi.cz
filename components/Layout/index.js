dbee0504c66e4749f075cb2182ce94c8980da5db
Jan Radl <jan.radl@citrix.com>
2019-12-03 14:55:02 +0100

    GA

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -4,36 +4,55 @@ import Cursor from "../Cursor";
 import styles from "./Layout.scss";
 import { PostHome, PostDetail } from "../Post";
 import classnames from "classnames";
+import ReactGA from "react-ga";
 
 const d = new Date();
 const n = d.getFullYear();
 
-export const LayoutHome = ({ data }) => (
-  <>
-    <Cursor />
-    <div className={styles["page"]}>
-      <Head />
-      <Header type="home" />
-      <main className={classnames(styles["main"], styles["main--home"])}>
-        {data.map(p => (
-          <PostHome key={p.order} post={p} />
-        ))}
-      </main>
-      <footer className={styles["main__footer"]}>
-        &copy; 2017–{n} venkuzaseprsi.cz
-      </footer>
-    </div>
-  </>
-);
+export class LayoutHome extends React.Component {
+  componentDidMount() {
+    ReactGA.initialize("UA-50377630-2");
+  }
+  render() {
+    return (
+      <>
+        <Cursor />
+        <div className={styles["page"]}>
+          <Head />
+          <Header type="home" />
+          <main className={classnames(styles["main"], styles["main--home"])}>
+            {this.props.data.map(p => (
+              <PostHome key={p.order} post={p} />
+            ))}
+          </main>
+          <footer className={styles["main__footer"]}>
+            &copy; 2017–{n} venkuzaseprsi.cz
+          </footer>
+        </div>
+      </>
+    );
+  }
+}
 
-export const LayoutPost = ({ data }) => (
-  <>
-    <Cursor />
-    <div className={styles["page"]}>
-      <Head title={data.title} excerpt={data.excerpt} image={data.image} />
-      <main className={classnames(styles["main"], styles["main--post"])}>
-        <PostDetail post={data} />
-      </main>
-    </div>
-  </>
-);
+export class LayoutPost extends React.Component {
+  componentDidMount() {
+    ReactGA.initialize("UA-50377630-2");
+  }
+  render() {
+    return (
+      <>
+        <Cursor />
+        <div className={styles["page"]}>
+          <Head
+            title={this.props.data.title}
+            excerpt={this.props.data.excerpt}
+            image={this.props.data.image}
+          />
+          <main className={classnames(styles["main"], styles["main--post"])}>
+            <PostDetail post={this.props.data} />
+          </main>
+        </div>
+      </>
+    );
+  }
+}
